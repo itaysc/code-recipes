8604aeef6bcb605b34642f906a0d53b225e4ed6b
itayschmidt <itay.s@bookaway.com>
2021-09-02 09:10:39 +0300

    start working on common module

---
+++ nats-test/src/nats-wrapper.ts
@@ -0,0 +1,39 @@
+import nats, { Stan } from 'node-nats-streaming';
+
+class NatsWrapper {
+    private _client? : Stan;
+    get client() {
+        if(!this._client) {
+            throw new Error('Cannot access NATS client before connecting');
+        }
+
+        return this._client;
+    }
+
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, { url });
+        return new Promise<void>((resolve, reject) => {
+            this.client.on('connect', () => {
+                resolve();
+            });
+            this.client.on('error', (err) => {
+                reject(err);
+            })
+        })
+    }
+
+}
+
+export const natsWrapper = new NatsWrapper();
+
+/**
+usage example: 
+await natsWrapper.connect('user-service', '123', 'http://nats-service:4222')
+also add graceful shutdown
+natsWrapper.client.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+})
+process.on('SIGINT', () => natsWrapper.client.close()); 
+process.on('SIGTERM', () => natsWrapper.client.close());
+*/
